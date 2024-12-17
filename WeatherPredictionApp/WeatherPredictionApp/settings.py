ddb1c9025f61106f4c45538bafeffd428c93910a
khalboy <genkhal25@gmail.com>
2024-12-17 15:16:37 +0100

    almost done configuring app

--- WeatherPredictionApp/WeatherPredictionApp/settings.py
+++ WeatherPredictionApp/WeatherPredictionApp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+ 'myapp.apps.myappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
