5fdc91d8c4304b17b79bc39efd7c573928c46706
khalboy <genkhal25@gmail.com>
2024-12-23 10:57:43 +0100

    almost done configuring app

--- WeatherPredictionApp/WeatherPredictionApp/settings.py
+++ WeatherPredictionApp/WeatherPredictionApp/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
- 'myapp.apps.myappConfig',
+ 'myapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
